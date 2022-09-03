e7297c80c95c48c13ab16d211b8c3476b0ca53eb
Yasin Cepni <yasincepnioglu@gmail.com>
2022-09-03 18:10:02 +0200

    modified:   src/app/board/board.component.html 	modified:   src/app/board/board.component.ts

--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -27,12 +27,14 @@ export class BoardComponent implements OnInit {
   }
 
   makeMove(idx: number) {
+    if (!this.winner){
     if (!this.squares[idx]) {
       this.squares.splice(idx, 1, this.player);
       this.xIsNext = !this.xIsNext;
     }
+    this.winner = this.calculateWinner();}
 
-    this.winner = this.calculateWinner();
+    
   }
 
   calculateWinner() {
