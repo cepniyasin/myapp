233264ed92e2680d4fe599485ab18a1218627e25
Yasin Cepni <yasincepnioglu@gmail.com>
2022-09-04 16:52:18 +0200

    modified:   src/app/board/board.component.ts

--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -27,17 +27,94 @@ export class BoardComponent implements OnInit {
   }
 
   makeMove(idx: number) {
-    if (!this.winner){
+        if (!this.winner){
     if (!this.squares[idx]) {
       this.squares.splice(idx, 1, this.player);
       this.xIsNext = !this.xIsNext;
-    }
-    this.winner = this.calculateWinner();}
 
+    }
+    this.winner = this.calculateWinner(this.squares);
+
+    if (!this.winner){this.opponentMove()}
+    }
     
   }
 
-  calculateWinner() {
+////// My main additions start. /////
+
+  opponentMove(){
+    let possibleMoves = this.findPossibleMoves(this.squares)
+    let winningMove = this.checkWinningMove(possibleMoves);
+    if(winningMove){
+      this.squares.splice(winningMove, 1, 'O');
+      this.winner = this.calculateWinner(this.squares)
+      this.xIsNext = !this.xIsNext;
+      return
+    }
+
+    let blockingMove = this.checkBlockingMove(possibleMoves);
+    if (blockingMove){
+      this.squares.splice(blockingMove, 1, 'O');
+      this.xIsNext = !this.xIsNext;
+      return
+    }
+
+    this.makeRandomMove(possibleMoves);
+    this.xIsNext = !this.xIsNext;
+    return 
+  }
+
+  findPossibleMoves(arr){
+    let possibleMoveIndices = []
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] == null){
+        possibleMoveIndices.push(i)
+      }
+    }
+    console.log(possibleMoveIndices)
+    return possibleMoveIndices
+  }
+
+  checkWinningMove(arr){
+    for (let i = 0; i < arr.length; i++) {
+        let temp_arr = [...this.squares]
+        temp_arr[arr[i]] = "O"
+        if (this.calculateWinner(temp_arr)){
+          console.log(arr[i])
+          return arr[i]
+        }
+    }
+    return null
+  }
+  
+  checkBlockingMove(arr){
+    for (let i = 0; i < arr.length; i++) {
+      let temp_arr = [...this.squares]
+      temp_arr[arr[i]] = "X"
+      if (this.calculateWinner(temp_arr)){
+        console.log(arr[i])
+        return arr[i]
+      }
+  }
+  return null
+  }
+
+  makeRandomMove(arr){
+    let numberOfPossibleMoves = arr.length
+    let randomIndex = this.getRandomInt(numberOfPossibleMoves)
+    this.squares.splice(randomIndex, 1, 'O');
+      return
+  }
+  
+  getRandomInt(max:number) {
+    return Math.floor(Math.random() * max);
+  }
+
+  
+////// My additions end. /////
+
+
+  calculateWinner(arr) {
     const lines = [
       [0, 1, 2],
       [3, 4, 5],
@@ -51,11 +128,11 @@ export class BoardComponent implements OnInit {
     for (let i = 0; i < lines.length; i++) {
       const [a, b, c] = lines[i];
       if (
-        this.squares[a] &&
-        this.squares[a] === this.squares[b] &&
-        this.squares[a] === this.squares[c]
+        arr[a] &&
+        arr[a] === arr[b] &&
+        arr[a] === arr[c]
       ) {
-        return this.squares[a];
+        return arr[a];
       }
     }
     return null;
